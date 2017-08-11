7120436b828bbadd3802908ff515b783f3a20efa
Jakub Rozlivek <rozlijak@fel.cvut.cz>
2017-08-11 13:15:11 +0200

    clean repo

--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jacob/CLionProjects/pursuit/lib/googletest
-# Build directory: C:/Users/Jacob/CLionProjects/pursuit/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
