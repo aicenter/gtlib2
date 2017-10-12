023f7a96db5dfc8795ef7a51a8a09cc1f9226cb5
Jakub Rozlivek <rozlijak@fel.cvut.cz>
2017-10-12 16:51:12 +0200

    New makefile

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 //#include <gtest/gtest.h>
 
 #include "efg.h"
-#include "normalFormLP.h"
+#include "cplexfiles/normalFormLP.h"
 
 int main(int argc, char* argv[]) {
   srand(static_cast<unsigned int>(time(nullptr)));
