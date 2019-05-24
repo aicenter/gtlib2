543be0c72ac8b7cfde0e56b61f77e58a0fef89c0
Matej Sestak <sestamat@fel.cvut.cz>
2019-05-24 11:25:32 +0200

    Added basic test

--- tests/randomGameTest.cpp
+++ tests/randomGameTest.cpp
@@ -34,4 +34,101 @@ namespace GTLib2::domains {
 using algorithms::DomainStatistics;
 using algorithms::treeWalkEFG;
 
+RandomGameDomain testDomain[] = {
+RandomGameDomain({.seed = 13, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 4, .maxRewardModification = 2, .maxUtility = 100, .binaryUtility = true, .utilityCorrelation = true, .fixedBranchingFactor = true}),
+RandomGameDomain({.seed = 7, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 2, .maxRewardModification = 2, .maxUtility = 100, .binaryUtility = true, .utilityCorrelation = true, .fixedBranchingFactor = true}),
+RandomGameDomain({.seed = 5, .maxDepth = 2, .maxBranchingFactor = 6, .maxDifferentObservations = 2, .maxRewardModification = 2, .maxUtility = 100, .binaryUtility = true, .utilityCorrelation = true, .fixedBranchingFactor = false}),
+RandomGameDomain({.seed = 9, .maxDepth = 3, .maxBranchingFactor = 6, .maxDifferentObservations = 3, .maxRewardModification = 20, .maxUtility = 100, .binaryUtility = false, .utilityCorrelation = true, .fixedBranchingFactor = false}),
+RandomGameDomain({.seed = 17, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 2, .maxRewardModification = 20, .maxUtility = 100, .binaryUtility = false, .utilityCorrelation = true, .fixedBranchingFactor = false}),
+RandomGameDomain({.seed = 1, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 2, .maxRewardModification = 2, .maxUtility = 100, .binaryUtility = true, .utilityCorrelation = false, .fixedBranchingFactor = false}),
+RandomGameDomain({.seed = 3, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 2, .maxRewardModification = 20, .maxUtility = 100, .binaryUtility = false, .utilityCorrelation = true, .fixedBranchingFactor = false}),
+RandomGameDomain({.seed = 13, .maxDepth = 3, .maxBranchingFactor = 4, .maxDifferentObservations = 2, .maxRewardModification = 2, .maxUtility = 100, .binaryUtility = false, .utilityCorrelation = false, .fixedBranchingFactor = false}),
+
+};
+
+TEST(RandomGame, checkDomainStats){
+
+    vector<DomainStatistics> expectedStats = {
+        {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 341,
+            .num_terminals = 256,
+            .num_states = 273,
+            .num_histories = {17, 68},
+            .num_infosets = {17, 17},
+            .num_sequences = {69, 69},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 341,
+            .num_terminals = 256,
+            .num_states = 273,
+            .num_histories = {17, 68},
+            .num_infosets = {9, 9},
+            .num_sequences = {37, 37},
+        }, {
+            .max_EFGDepth = 2,
+            .max_StateDepth = 2,
+            .num_nodes = 13,
+            .num_terminals = 9,
+            .num_states = 10,
+            .num_histories = {1, 3},
+            .num_infosets = {1, 1},
+            .num_sequences = {4, 4},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 629,
+            .num_terminals = 489,
+            .num_states = 515,
+            .num_histories = {26, 114},
+            .num_infosets = {16, 16},
+            .num_sequences = {72, 72},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 175,
+            .num_terminals = 112,
+            .num_states = 129,
+            .num_histories = {17, 46},
+            .num_infosets = {9, 9},
+            .num_sequences = {26, 26},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 51,
+            .num_terminals = 33,
+            .num_states = 38,
+            .num_histories = {5, 13},
+            .num_infosets = {5, 5},
+            .num_sequences = {14, 14},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 37,
+            .num_terminals = 21,
+            .num_states = 26,
+            .num_histories = {5, 11},
+            .num_infosets = {5, 5},
+            .num_sequences = {12, 12},
+        }, {
+            .max_EFGDepth = 4,
+            .max_StateDepth = 3,
+            .num_nodes = 159,
+            .num_terminals = 98,
+            .num_states = 115,
+            .num_histories = {17, 44},
+            .num_infosets = {9, 9},
+            .num_sequences = {25, 25},
+        },
+    };
+
+    for (int i = 0; i < expectedStats.size(); ++i) {
+        cout << ">> checking domain [" << i << "] " << testDomain[i].getInfo() << endl;
+        DomainStatistics actualStats;
+        calculateDomainStatistics(testDomain[i], &actualStats);
+        EXPECT_EQ(actualStats, expectedStats[i]);
+    }
+}
 }
