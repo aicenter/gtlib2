f22f56f7a7cc8043a3b36f3552567aee17bdb8ed
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2019-05-30 12:23:16 +0200

    Add range test

---
+++ tests/rangeTest.cpp
@@ -0,0 +1,38 @@
+/*
+    Copyright 2019 Faculty of Electrical Engineering at CTU in Prague
+
+    This file is part of Game Theoretic Library.
+
+    Game Theoretic Library is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation, either version 3
+    of the License, or (at your option) any later version.
+
+    Game Theoretic Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Game Theoretic Library.
+
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#include "base/base.h"
+#include "utils/range.h"
+
+#include "gtest/gtest.h"
+
+
+namespace GTLib2::utils {
+
+TEST(Range, LogRange) {
+    EXPECT_EQ(logRange(1., 10000., 5, 10.), (vector<double>{1., 10., 100., 1000., 10000.}));
+    EXPECT_EQ(logRange(10., 10000., 4, 10.), (vector<double>{10., 100., 1000., 10000.}));
+    EXPECT_EQ(logRange(10., 100., 3, 10.), (vector<double>{10., 31.622776601683793, 100.}));
+}
+
+}
